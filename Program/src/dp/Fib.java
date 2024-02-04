fa2970fe6d450f1964fe98fbd707187192f672e6
Kimihiru <152416990+Kimihiru@users.noreply.github.com>
2024-02-04 23:07:14 +0800

    Add files via upload

---
+++ Program/src/dp/Fib.java
@@ -0,0 +1,32 @@
+package dp;
+
+import java.util.Scanner;
+
+/**
+ * @author Sakura
+ * dp解斐波那契数列
+ * 分治解斐波那契数列
+ */
+public class Fib {
+    public static int fib(int n){
+        return n<2? n:fib(n-1)+fib(n-2);
+    }
+    public static int dpFib(int n){
+        if(n<2){
+            return n;
+        }
+        int[] dp=new int[n+1];
+        dp[0]=1;dp[1]=1;
+        for (int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("输入n:");
+        int n=sc.nextInt();
+        System.out.println(dpFib(n));
+        sc.close();
+    }
+}
